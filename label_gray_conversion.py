206749ed60f50edae5dbe65f1727b2022ca24b18
Christian Rauch <Christian.Rauch@ed.ac.uk>
2017-02-14 20:03:48 +0000

    colour label to gray level conversion

--- label_gray_conversion.py
+++ label_gray_conversion.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-# convert gray into colour labels
-# ./label_colour_conversion.py <path_to_files> <path_to_colour_map> <file_pattern>
+# convert colour into gray labels
+# ./label_gray_conversion.py <path_to_files> <path_to_colour_map> <file_pattern>
 
 from __future__ import absolute_import, division, print_function
 
@@ -18,32 +18,29 @@ import multiprocessing
 
 def label_to_colour(f):
     img = io.imread(f)
-    # determine if image is colour or gray label, check if all channels are equal
-    # we need img.ndim==3 for lazy evaluation (to not to test 'np.array_equal' on a not avilable 3rd channel)
-    if img.ndim==3 and ((not np.array_equal(img[:, :, 0],img[:, :, 1])) and (not np.array_equal(img[:, :, 0],img[:, :, 2]))):
+    # check for colour image
+    if img.ndim!=3:
         print(f)
-        print("not a gray level image!")
+        print("not a colour image!")
         return
-    elif img.ndim==3:
-        # gray image with 3 channels
-        img = img[:, :, 0]
 
     # create new label image with colours instead of gray levels
-    cimg = np.empty((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    gimg = np.empty((img.shape[0], img.shape[1], 1), dtype=np.uint)
 
     for k in colours.keys():
-        cimg[img==k,:] = colours[k]
+        gimg[np.all(img == k, axis=2)] = colours[k]
+        gimg = np.squeeze(gimg)
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        io.imsave(f, cimg)
+        io.imsave(f, gimg)
 
 # read links and label mappings (gray <-> colour)
 linkfile = csv.reader(open(sys.argv[2], 'r'), delimiter=' ' )
 colours = dict()
 for line in linkfile:
-    colours[int(line[1])] = [int(float(line[2])*255), int(float(line[3])*255), int(float(line[4])*255)]
-colours[0] = [0, 0, 0]
+    colours[tuple([int(float(line[2]) * 255), int(float(line[3]) * 255), int(float(line[4]) * 255)])] = int(line[1])
+colours[tuple([0, 0, 0])] = 0
 
 # convert files in parallel
 if len(sys.argv)<4:
